57f6cc4292ec431eebc491839daa39eaa8bc111e
Peter Cornelius <pcc@gmx.net>
2024-02-17 09:21:34 +0100

    ports-mgmt/py-pytoport: Adjusted MASTER_SITES in created Makefile after CHEESESHOP to PYPI transition ports-mgmt/py-pytoport: Removed outdated $ FreeBSD $ from created Makefiles 	modified:   ports-mgmt/py-pytoport/files/patch-pytoport_____init____.py

--- ports-mgmt/py-pytoport/files/patch-pytoport_____init____.py
+++ ports-mgmt/py-pytoport/files/patch-pytoport_____init____.py
@@ -1,6 +1,6 @@
---- pytoport/__init__.py.orig	2019-06-04 03:51:47 UTC
-+++ pytoport/__init__.py
-@@ -124,7 +124,7 @@ def get_minimum(data):
+--- pytoport/__init__.py.orig	2019-06-04 05:51:47.000000000 +0200
++++ pytoport/__init__.py	2024-02-16 00:14:43.581972000 +0100
+@@ -124,7 +124,7 @@
          lowest = supported[0]
  
      if lowest[1] == -1:
@@ -9,7 +9,22 @@
      else:
          ver = "%s.%s" % lowest
  
-@@ -167,7 +167,12 @@ def generate_makefile(data, path=os.getcwd(), name=Non
+@@ -153,12 +153,12 @@
+     o.write("# Created by: ")
+     if name is not None and email is not None:
+         o.write("%s <%s>" % (name, email))
+-    o.write("\n# $FreeBSD$\n\n")
++    o.write("\n\n")
+ 
+     add(o, "PORTNAME", info['name'].lower())
+     add(o, "PORTVERSION", info['version'])
+     add(o, "CATEGORIES", "devel python")
+-    add(o, "MASTER_SITES", "CHEESESHOP")
++    add(o, "MASTER_SITES", "PYPI")
+     add(o, "PKGNAMEPREFIX", "${PYTHON_PKGNAMEPREFIX}")
+     o.write('\n')
+ 
+@@ -167,7 +167,12 @@
      else:
          add(o, "MAINTAINER", email)
      summary = info.get('summary', '# FILL ME')
